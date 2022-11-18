8e783e8dcfec06e3f1b8500a1e2722ef64baeadc
nsai22z-20 <nsai22z-20@sr.dmcs.pl>
2022-11-18 23:41:33 +0100

    Simplify Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,92 +10,56 @@ import PersonIcon from '@mui/icons-material/Person';
 import DatasetIcon from '@mui/icons-material/Dataset';
 import SummarizeIcon from '@mui/icons-material/Summarize';
 import HowToVoteIcon from '@mui/icons-material/HowToVote';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { ReactNode } from 'react';
+
+type NavigationButtonProps = {
+  link: string;
+  icon: ReactNode;
+  label: string;
+};
+
+const NavigationButton = (props: NavigationButtonProps) => {
+  const location = useLocation();
+  const Content = (props: NavigationButtonProps) => (
+    <MenuItem className={location.pathname === props.link ? 'active' : ''}>
+      <ListItemIcon>{props.icon}</ListItemIcon>
+      <ListItemText>{props.label}</ListItemText>
+    </MenuItem>
+  );
+  return (
+    <>
+      {props.link.includes('http') ? (
+        <a href={props.link}>
+          <Content {...props} />
+        </a>
+      ) : (
+        <Link to={props.link}>
+          <Content {...props} />
+        </Link>
+      )}
+    </>
+  );
+};
 
 const Navigation = () => {
   return (
     <NavigationStyled>
       <MenuList sx={{ display: 'flex', flexDirection: 'column' }}>
-        <a href="https://virtul.p.lodz.pl/virtul/">
-          <MenuItem>
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText>virTul</ListItemText>
-          </MenuItem>
-        </a>
-        <Link to="/bulletinBoard">
-          <MenuItem>
-            <ListItemIcon>
-              <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText>Bulletin board</ListItemText>
-          </MenuItem>
-        </Link>
-        <Link to="/schedule">
-          <MenuItem>
-            <ListItemIcon>
-              <ScheduleIcon />
-            </ListItemIcon>
-            <ListItemText>Course schedule</ListItemText>
-          </MenuItem>
-        </Link>
-        <Link to="/personalData">
-          <MenuItem>
-            <ListItemIcon>
-              <PersonIcon />
-            </ListItemIcon>
-            <ListItemText>Personal Info</ListItemText>
-          </MenuItem>
-        </Link>
-        <Link to="/generalData">
-          <MenuItem>
-            <ListItemIcon>
-              <DatasetIcon />
-            </ListItemIcon>
-            <ListItemText>General data</ListItemText>
-          </MenuItem>
-        </Link>
-        <Link to="/reportCard">
-          <MenuItem>
-            <ListItemIcon>
-              <SummarizeIcon />
-            </ListItemIcon>
-            <ListItemText>Report card</ListItemText>
-          </MenuItem>
-        </Link>
-        <Link to="/electiveCourses">
-          <MenuItem>
-            <ListItemIcon>
-              <HowToVoteIcon />
-            </ListItemIcon>
-            <ListItemText>Elective courses</ListItemText>
-          </MenuItem>
-        </Link>
-        <a href="https://programy.p.lodz.pl/ectslabel-web/">
-          <MenuItem>
-            <ListItemIcon>
-              <SchoolIcon />
-            </ListItemIcon>
-            <ListItemText>ECTS Course catalogue</ListItemText>
-          </MenuItem>
-        </a>
-        <Link to="/terms">
-          <MenuItem>
-            <ListItemIcon>
-              <CalendarMonthIcon />
-            </ListItemIcon>
-            <ListItemText>Terms</ListItemText>
-          </MenuItem>
-        </Link>
-        <a href="https://samorzad.p.lodz.pl/">
-          <MenuItem>
-            <ListItemIcon>
-              <GroupsIcon />
-            </ListItemIcon>
-            <ListItemText>Student government</ListItemText>
-          </MenuItem>
-        </a>
+        <NavigationButton link="https://virtul.p.lodz.pl/virtul/" icon={<HomeIcon />} label="virTul" />
+        <NavigationButton link="/bulletinBoard" icon={<DashboardIcon />} label="Bulletin board" />
+        <NavigationButton link="/schedule" icon={<ScheduleIcon />} label="Course schedule" />
+        <NavigationButton link="/personalData" icon={<PersonIcon />} label="Personal Info" />
+        <NavigationButton link="/generalData" icon={<DatasetIcon />} label="General data" />
+        <NavigationButton link="/reportCard" icon={<SummarizeIcon />} label="Report card" />
+        <NavigationButton link="/electiveCourses" icon={<HowToVoteIcon />} label="Elective courses" />
+        <NavigationButton
+          link="https://programy.p.lodz.pl/ectslabel-web/"
+          icon={<SchoolIcon />}
+          label="ECTS Course catalogue"
+        />
+        <NavigationButton link="/terms" icon={<CalendarMonthIcon />} label="Terms" />
+        <NavigationButton link="https://samorzad.p.lodz.pl/" icon={<GroupsIcon />} label="Student government" />
       </MenuList>
     </NavigationStyled>
   );
